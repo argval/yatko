5d1f46fb8abd444b7c7bb51c5a503d8a069f26ab
argval <anuragr.vallur@gmail.com>
2026-09-01 17:06:09 +0530

    Revert "refactor: simplify homepage navigation"
    This reverts commit 52d21bfca6ff9f179180c85d9decff143577e14e.

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,4 @@
 import { HomeSearchSection } from "./home-search-section";
-import { HomeExamples } from "./home-examples";
 import { HomeHowItWorks } from "./home-how-it-works";
 import { SITE_DOMAIN } from "@/lib/site";
 
@@ -29,7 +28,6 @@ export default function Home() {
 
         <div className="space-y-4">
           <HomeSearchSection />
-          <HomeExamples />
         </div>
 
         <a
